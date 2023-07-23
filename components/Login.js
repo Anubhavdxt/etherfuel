768bc12305358423cc792425b93c66c2c7f72f46
Anubhav <anubhavdxt46@gmail.com>
2023-07-23 12:05:36 +0530

    Remove unwanted files

--- components/Login.js
+++ components/Login.js
@@ -1,111 +1,111 @@
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
-import contractAbi from '../utils/contractABI.json';
-import styles from '/styles/Login.module.css';
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import contractAbi from '../utils/contractABI.json'
+import styles from '/styles/Login.module.css'
 
 // Add the domain you will be minting
-const tld = '.etherfuel';
-const CONTRACT_ADDRESS = '0x71FB6D6EeaD55E394808E0E6bDE24B66f4aB5814';
+const tld = '.etherfuel'
+const CONTRACT_ADDRESS = '0x71FB6D6EeaD55E394808E0E6bDE24B66f4aB5814'
 
 const Login = () => {
-  const [currentAccount, setCurrentAccount] = useState('');
-  const [domain, setDomain] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [record, setRecord] = useState('');
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [domain, setDomain] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [record, setRecord] = useState('')
 
   const connectWallet = async () => {
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
 
       if (!ethereum) {
-        alert('Get MetaMask -> https://metamask.io/');
-        return;
+        alert('Get MetaMask -> https://metamask.io/')
+        return
       }
 
       const accounts = await ethereum.request({
         method: 'eth_requestAccounts',
-      });
+      })
 
-      console.log('Connected', accounts[0]);
-      setCurrentAccount(accounts[0]);
+      console.log('Connected', accounts[0])
+      setCurrentAccount(accounts[0])
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const checkIfWalletIsConnected = async () => {
-    const { ethereum } = window;
+    const { ethereum } = window
 
     if (!ethereum) {
-      alert('Make sure you have metamask!');
-      return;
+      alert('Make sure you have metamask!')
+      return
     } else {
-      console.log('We have the ethereum object', ethereum);
+      console.log('We have the ethereum object', ethereum)
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    const accounts = await ethereum.request({ method: 'eth_accounts' })
     if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log('Found an authorized account:', account);
-      setCurrentAccount(account);
+      const account = accounts[0]
+      console.log('Found an authorized account:', account)
+      setCurrentAccount(account)
     } else {
-      alert('No authorized account found');
+      alert('No authorized account found')
     }
-  };
+  }
 
   const mintDomain = async () => {
     if (!domain) {
-      return;
+      return
     }
     if (domain.length < 3) {
-      alert('Domain must be at least 3 characters long');
-      return;
+      alert('Domain must be at least 3 characters long')
+      return
     }
 
     const price =
-      domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
-    console.log('Minting domain', domain, 'with price', price);
+      domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1'
+    console.log('Minting domain', domain, 'with price', price)
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
         const contract = new ethers.Contract(
           CONTRACT_ADDRESS,
           contractAbi.abi,
           signer
-        );
+        )
 
-        console.log('Going to pop wallet now to pay gas...');
+        console.log('Going to pop wallet now to pay gas...')
         let tx = await contract.register(domain, {
           value: ethers.utils.parseEther(price),
-        });
+        })
 
-        const receipt = await tx.wait();
+        const receipt = await tx.wait()
 
         if (receipt.status === 1) {
           console.log(
             'Domain minted! https://mumbai.polygonscan.com/tx/' + tx.hash
-          );
+          )
 
-          tx = await contract.setRecord(domain, record);
-          await tx.wait();
+          tx = await contract.setRecord(domain, record)
+          await tx.wait()
 
           console.log(
             'Record set! https://mumbai.polygonscan.com/tx/' + tx.hash
-          );
+          )
 
-          setRecord('');
-          setDomain('');
+          setRecord('')
+          setDomain('')
         } else {
-          alert('Transaction failed! Please try again');
+          alert('Transaction failed! Please try again')
         }
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const renderNotConnectedContainer = () => (
     <div className={styles.connect_wallet_container}>
@@ -116,11 +116,14 @@ const Login = () => {
         Connect Wallet
       </button>
     </div>
-  );
+  )
 
   const renderInputForm = () => {
     return (
       <div className="flex flex-col items-center justify-center w-full">
+        <div className=" text-secondary px-8 py-6 font-semibold text-4xl mt-5 w-96 text-center">
+          <h1>Register your EV</h1>
+        </div>
         <div className="flex relative mb-3 items-center w-full">
           <input
             className="bg-tertiary text-secondary px-8 py-6 font-semibold text-2xl mt-5 w-96"
@@ -150,12 +153,12 @@ const Login = () => {
           Mint
         </button>
       </div>
-    );
-  };
+    )
+  }
 
   useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
+    checkIfWalletIsConnected()
+  }, [])
 
   return (
     <div className={styles.App}>
@@ -164,7 +167,7 @@ const Login = () => {
         {currentAccount && renderInputForm()}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,30 +1,25 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
     <div className="flex flex-row justify-between items-center px-48 text-secondary py-0">
       <div className="logo text-4xl opacity-85 uppercase font-semibold bg">
         <Link href="/">
-          <Image
-            src="/logo.svg"
-            height={180}
-            width={180}
-            alt="Logo"
-          ></Image>
+          <Image src="/logo.svg" height={180} width={180} alt="Logo"></Image>
         </Link>
       </div>
       <div className="flex flex-row justify-between items-center lowercase opacity-80 text-2xl">
-        {/* <div className="mr-16 link">
+        <div className="mr-16 link">
           <Link href="#projects">my projects</Link>
         </div>
         <div className="mr-16 link">
           <Link href="#about">about me</Link>
-        </div> */}
+        </div>
         <div className="link">
-          <Link href="/login">get started</Link>
+          <Link href="/login">register your EV</Link>
         </div>
       </div>
     </div>
-  );
+  )
 }
